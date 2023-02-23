9b57c0b2e30796fedd90551a0b0583a7060fac3b
“Raksh-Agg” <rakshit.agg04@gmail.com>
2023-02-24 03:23:24 +0530

    Task 2 (Time Complexity)

--- Lab4/insertionSort/Driver.c
+++ Lab4/insertionSort/Driver.c
@@ -3,17 +3,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+// Code for measuring executing time
+
+
 //  input.csv is The input file given. Make sure the file's first line contains the no.of lines of input to be given to the code, and the next lines each contatining input..
 int main()
 {
-    FILE* fptr = fopen ("input.csv", "r");
+    //  Enter which csv file you want to execute on prompt
+    char file_name[25];
+    scanf("%s", &file_name);
+    FILE* fptr = fopen (file_name, "r");
     int size;
     fscanf(fptr,"%d",&size);
     PerPtr Array = (PerPtr)malloc(size * sizeof(Person));
     char line[75];
     char *token;
     fgets(line, 75, fptr);
-    // Test string for storing name correctly
+    // Test string for storing name of Person correctly
     char * test_string;
     // Code for taking input from file for Person
     for (int j = 0 ; j < size ; j++)
@@ -34,7 +40,5 @@ int main()
         token = strtok(NULL, ",");
     }
     insertionSort(Array, size);
-    for (int i = 0 ; i < size ; i++)
-    print(*(Array+i));
     return 0;
 }
--- Lab4/insertionSort/insSort.c
+++ Lab4/insertionSort/insSort.c
@@ -1,9 +1,15 @@
 #include "insSort.h"
 #include <stdio.h>
+#include <time.h>
 void insertionSort(PerPtr arr, int size)
 {
+    __clock_t tv_begin;
+    __clock_t tv_end;
+    tv_begin = clock();
     for (int i = 0 ; i < size ; i++)
     insertInOrder(arr[i], arr, i);
+    tv_end = clock();
+    printf("No.of microsecond = %ld\n", (tv_end - tv_begin) );
 }
 void insertInOrder (Person ele, PerPtr arr, int index)
 {
