23a97cc42eb6beadcea1f9df50c55979ffe4c67a
“Raksh-Agg” <rakshit.agg04@gmail.com>
2023-03-01 22:37:09 +0530

    Task 4, Tail Rec

--- Lab5/input_files/LL_reader.c
+++ Lab5/input_files/LL_reader.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+struct timeval t1, t2;
+double time_taken;
 
 
 typedef struct node * NODE;
@@ -36,17 +39,17 @@ NODE createNewNode(int value)
 }
 
 // Recursive function to compute sum of integers in a linked list
-int llSumNTR(NODE head)
+long long llSumNTR(NODE head, long long mySum)
 {
     if (head == NULL)
-        return 0;
-    return head->ele + llSumNTR(head->next); // Pay close attention here
+        return mySum;
+    return llSumNTR(head->next, mySum + head->ele); // Pay close attention here
 }
 
 // This is just a wrapper function to take LIST as input, but run the recursive sum function on its NODEs starting from the head
-int llSumNTRWrapper(LIST list)
+long long llSumNTRWrapper(LIST list)
 {
-    return llSumNTR(list->head);
+    return llSumNTR(list->head, 0);
 }
 
 // Driver code
@@ -54,7 +57,7 @@ int main()
 {
     // Reads the file numbers1000.txt and creates a linked list with those integers
     FILE *fp;
-    fp = fopen("numbers1000.txt", "r");
+    fp = fopen("numbers1000000.txt", "r");
     if (fp == NULL)
     {
         printf("Error opening file\n");
@@ -73,12 +76,20 @@ int main()
 
     // Perform some operations on the linked list
     // ...
+    gettimeofday(&t1, NULL);
+
+    printf("%lld\n", llSumNTRWrapper(myList));
+
+    gettimeofday(&t2, NULL);
+    time_taken = (t2.tv_sec - t1.tv_sec) * 1e6;
+    time_taken = (time_taken + (t2.tv_usec - t1.tv_usec)) * 1e-6;
+    printf("Taking Sum took %f seconds to execute\n", time_taken);
 
     // Example operation: Print the elements of the linked list
-    NODE temp = myList->head;
-    while (temp != NULL)
-    {
-        printf("%d ", temp->ele);
-        temp = temp->next;
-    }
+    // NODE temp = myList->head;
+    // while (temp != NULL)
+    // {
+    //     printf("%d ", temp->ele);
+    //     temp = temp->next;
+    // }
 }
